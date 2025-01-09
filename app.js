802b12fe6f0e664d5badf070c581827d8b79cc2c
Unnirkrishnan KJ <116744073+learnfromscratches@users.noreply.github.com>
2025-01-09 11:58:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const fastify = require('fastify')({ logger: true });
-
-// Use the PORT environment variable or default to 3000
+// src/app.js
+require("dotenv").config();
+const fastify = require("fastify")({ logger: false });
 const PORT = process.env.PORT || 3000;
+const orderRoutes = require("./src/routes/orderRoutes");
+const webhookRoutes = require("./src/routes/webhookRoutes");
 
-fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
+fastify.register(orderRoutes);
+fastify.register(webhookRoutes);
+
+fastify.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
